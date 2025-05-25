b637de559215a6a5fb6e2dabf58dc4afb8a65261
RobertaBS09 <robertabarbosas123@gmail.com>
2025-05-24 21:58:17 -0300

    projeto finalizado 24/05

--- src/MapaRobo022.java
+++ src/MapaRobo022.java
@@ -9,9 +9,13 @@ public class MapaRobo022 {
         int etapa = 1;
         int passosNecessarios = 0;
         String direcaoCorreta = "";
-        int passosAcumulados = 0; // soma dos passos certos
+        int passosAcumulados = 0;
+        System.out.println("--------------------------");
         System.out.println("Potência da Bateria: " + bateria + "mA");
-        System.out.println("Consumo energético por PASSO: 1000mA\n");
+        System.out.println("Consumo energético por PASSO: 1000mA");
+        System.out.println("[AVISO] Os passos são acumulativos por etapa.");
+        System.out.println("--------------------------");
+
         while (bateria >= consumo && etapa <= 5) {
             switch (etapa) {
                 case 1:
@@ -35,45 +39,68 @@ public class MapaRobo022 {
                     passosNecessarios = 9;
                     break;
             }
+            System.out.println("[AVISO] Todos os passos digitados gastarão bateria (mesmo que a etapa precise de menos)");
             System.out.println("Etapa " + etapa + " - Bateria: " + bateria + "mA");
-            System.out.println("  ");
-            System.out.println("(D)ireita");
-            System.out.println("(E)squerda");
-            System.out.println("(F)rente");
-            System.out.println("(T)rás");
-            System.out.print("Digite a direção: ");
-            String direcao = sc.next().toLowerCase();
-            System.out.print("Digite a quantidade de passos: ");
-            int passos = sc.nextInt();
-            // transforma passo negativo em positivo
+            System.out.println("--------------------------");
+            System.out.println("(D)ireita | (E)squerda | (F)rente | (T)rás");
+
+
+            String direcao;
+            do {
+                System.out.print("Digite a direção: ");
+                direcao = sc.next().toLowerCase();
+                if (!direcao.equals("d") && !direcao.equals("e") && !direcao.equals("f") && !direcao.equals("t")) {
+                    System.out.println("Direção inválida! Tente novamente.\n");
+                }
+            } while (!direcao.equals("d") && !direcao.equals("e") && !direcao.equals("f") && !direcao.equals("t"));
+
+
+            int passos;
+            do {
+                System.out.print("Digite a quantidade de passos: ");
+                passos = sc.nextInt();
+                if (passos <= 0) {
+                    System.out.println("\nO NÚMERO DE PASSOS DEVE SER MAIOR QUE 1\n");
+                }
+            } while (passos <= 0);
+
             int passosRealizados = Math.abs(passos);
-            // verifica se tem energia suficiente para os passos
+
+
             if (bateria >= passosRealizados * consumo) {
                 bateria -= passosRealizados * consumo;
-                System.out.println("\n--------------------------");
+                System.out.println("--------------------------");
                 System.out.println(passosRealizados + " passos dados. Bateria restante: " + bateria + "mA");
-                System.out.println("--------------------------\n");
+                System.out.println("--------------------------");
             } else {
-                System.out.println("Bateria insuficiente para realizar " + passosRealizados + " passos.");
-                bateria = 0;
-                break;
+                passosRealizados = bateria / consumo;
+                bateria -= passosRealizados * consumo;
+                System.out.println("--------------------------");
+                System.out.println("Bateria insuficiente! Serão dados apenas " + passosRealizados + " passos.");
+                System.out.println(passosRealizados + " passos dados. Bateria restante: " + bateria + "mA");
+                System.out.println("--------------------------");
             }
-            //se a direção estiver correta, acumula os passos
-            if (direcao.equals(direcaoCorreta)) {
-                passosAcumulados += passos;
-                if (passosAcumulados >= passosNecessarios) {
-                    System.out.println("Movimento correto! Etapa " + etapa + " concluída.\n");
-                    etapa++;
-                    passosAcumulados = 0; // zera para próxima etapa
-                } else {
 
-                    System.out.println("Faltam passos para concluir a etapa.\n");
+
+            if (direcao.equals(direcaoCorreta)) {
+                passosAcumulados += passosRealizados;
+                if (passosAcumulados >= passosNecessarios) {
+                    System.out.println("Movimento correto! Etapa " + etapa + " concluída.");
+                    System.out.println("--------------------------");
+                    etapa++;
+                    passosAcumulados = 0;
+                } else {
+                    System.out.println("--------------------------");
+                    System.out.println("Faltam passos para concluir a etapa.");
+                    System.out.println("Passos acumulados na etapa atual: " + passosAcumulados);
+                    System.out.println("--------------------------");
                 }
             } else {
-                // direção errada gasta bateria, mas não acumula passos
-                System.out.println("Está indo pelo caminho errado!\n");
+
+                System.out.println("CAMINHO ERRADO!");
             }
         }
+
         if (etapa > 5) {
             System.out.println("Parabéns! Você chegou ao fim do caminho.");
         } else {
